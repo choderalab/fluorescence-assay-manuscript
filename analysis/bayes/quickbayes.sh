a0234144eab45523467e7951308d3e6a7f307258
sonyahanson <hansons@mskcc.org>
2016-07-31 19:48:48 -0400

    added quickmodel results

---
+++ analysis/bayes/quickbayes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script automatically generates pdf file and csv file summaries
+# of bayesian predictions of delG from xml data files.
+# REQUIREMENTS: assaytools must be installed
+
+#This is currently incomplete and only analyzes a small selection of the available data
+
+python inputs.py
+quickmodel
+
+
