a0c500233c99f981ffca44e59e01afeddb4ad706
sonyahanson <hansons@mskcc.org>
2019-10-08 13:41:38 +0200

    update delG compare plots

--- figures/Figure4/generate_Src_delG_compare.py
+++ figures/Figure4/generate_Src_delG_compare.py
@@ -1,9 +1,10 @@
 #Src WT and GK
 #Analysis conducted on lilac, here results are taken from output json files
 #quickmodel --inputs inputs_multiple_well_single_wv_Src_4ligs --type singlet --nsamples 100000
-# Just to note: 'P_error'       :  0.35,
+# Just to note: 'P_error'       :  0.15,
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 import seaborn as sns
 sns.set(style='white')
@@ -11,48 +12,59 @@ sns.set_context('talk')
 
 import matplotlib.lines as mlines
 
-# Src "DeltaG_cred_int": "$\\Delta G$ =  -26.6 [-34.2,-19.2] $k_B T$"
-Bos = [-34.2,-26.6,-19.2]
+# Import 95% credibility intervals from json file quickmodel results
+import json
+Bos_file = 'mwsw/Src-Bosutinib-AB-2019-10-04 09:04.json'
+GK_Bos_file = 'mwsw/Src_T338I-Bosutinib-AB-2019-10-04 09:05.json'
+Bsi_file = 'mwsw/Src-Bosutinib Isomer-CD-2019-10-04 14:42.json'
+GK_Bsi_file = 'mwsw/Src_T338I-Bosutinib Isomer-CD-2019-10-04 15:09.json'
+Erl_file = 'mwsw/Src-Erlotinib-EF-2019-10-04 20:22.json'
+GK_Erl_file = 'mwsw/Src_T338I-Erlotinib-EF-2019-10-04 21:00.json'
+Gef_file = 'mwsw/Src-Gefitinib-GH-2019-10-05 01:56.json'
+GK_Gef_file = 'mwsw/Src_T338I-Gefitinib-GH-2019-10-05 02:22.json'
 
-# SrcGK "DeltaG_cred_int": "$\\Delta G$ =  -26.6 [-34.1,-19.3] $k_B T$",
-GK_Bos = [-34.1,-26.6,-19.3]
+def json_parse(file):
+    with open(file) as json_file:
+        data = json.load(json_file)
+    three = data['DeltaG_cred_int'].split("=")[1].split(" $")[0].strip().split("[")
+    first = float(three[0].strip())
+    inner = three[1].split(",")
+    second = float(inner[0])
+    third = float(inner[1][:-1])
+    lig = [third,
+           first,
+           second]
 
-# "DeltaG_cred_int": "$\\Delta G$ =  -27 [-34.2,-19.7] $k_B T$"
-Bsi = [-34.2,-27,-19.7]
+    print(lig)
+    return(lig)
 
-# "DeltaG_cred_int": "$\\Delta G$ =  -15.1 [-20.7,-13.9] $k_B T$",
-GK_Bsi = [-20.7,-15.1,-13.9]
+Bos = json_parse(Bos_file)
+GK_Bos = json_parse(GK_Bos_file)
+Bsi = json_parse(Bsi_file)
+GK_Bsi = json_parse(GK_Bsi_file)
+Erl = json_parse(Erl_file)
+GK_Erl = json_parse(GK_Erl_file)
+Gef = json_parse(Gef_file)
+GK_Gef = json_parse(GK_Gef_file)
 
-# "DeltaG_cred_int": "$\\Delta G$ =  -14 [-15.3,-12.8] $k_B T$",
-Erl = [-15.3,-14,-12.8]
+labels = ['bosutinib','bosutinib isomer','erlotinib','gefitinib']
 
-# "DeltaG_cred_int": "$\\Delta G$ =  -14 [-15.9,-12.6] $k_B T$",
-GK_Erl = [-15.9,-14,-12.6]
+fig, ax = plt.subplots(figsize=(6,8))
 
-# "DeltaG_cred_int": "$\\Delta G$ =  -13.5 [-14.6,-12.6] $k_B T$",
-Gef = [-14.6,-13.5,-12.6]
+plt.errorbar(Bos[1],0.75,xerr=[[abs(Bos[1]-Bos[2])],[abs(Bos[1]-Bos[0])]], fmt='o',color='c',markersize=12)
+plt.errorbar(GK_Bos[1],1.25,xerr=[[abs(GK_Bos[1]-GK_Bos[2])],[abs(GK_Bos[1]-GK_Bos[0])]], fmt='X',color='c',markersize=12)
+plt.errorbar(Bsi[1],1.75,xerr=[[abs(Bsi[1]-Bsi[2])],[abs(Bsi[1]-Bsi[0])]], fmt='o',color='b',markersize=12)
+plt.errorbar(GK_Bsi[1],2.25,xerr=[[abs(GK_Bsi[1]-GK_Bsi[2])],[abs(GK_Bsi[1]-GK_Bsi[0])]], fmt='X',color='b',markersize=12)
+plt.errorbar(Erl[1],2.75,xerr=[[abs(Erl[1]-Erl[2])],[abs(Erl[1]-Erl[0])]], fmt='o',color='g',markersize=12)
+plt.errorbar(GK_Erl[1],3.25,xerr=[[abs(GK_Erl[1]-GK_Erl[2])],[abs(GK_Erl[1]-GK_Erl[0])]], fmt='X',color='g',markersize=12)
+plt.errorbar(Gef[1],3.75,xerr=[[abs(Gef[1]-Gef[2])],[abs(Gef[1]-Gef[0])]], fmt='o',color='purple',markersize=12)
+plt.errorbar(GK_Gef[1],4.25,xerr=[[abs(GK_Gef[1]-GK_Gef[2])],[abs(GK_Gef[1]-GK_Gef[0])]], fmt='X',color='purple',markersize=12)
 
-# "DeltaG_cred_int": "$\\Delta G$ =  -12.2 [-12.7,-11.8] $k_B T$"
-GK_Gef = [-12.7,-12.2,-11.8]
-
-labels = ['Bos','Bsi','Erl','Gef']
-
-fig, ax = plt.subplots(figsize=(8,4))
-
-plt.errorbar(0.75,Bos[1],yerr=[[abs(Bos[1]-Bos[2])],[abs(Bos[1]-Bos[0])]], fmt='o',color='c',markersize=12)
-plt.errorbar(1.25,GK_Bos[1],yerr=[[abs(GK_Bos[1]-GK_Bos[2])],[abs(GK_Bos[1]-GK_Bos[0])]], fmt='X',color='c',markersize=12)
-plt.errorbar(1.75,Bsi[1],yerr=[[abs(Bsi[1]-Bsi[2])],[abs(Bsi[1]-Bsi[0])]], fmt='o',color='b',markersize=12)
-plt.errorbar(2.25,GK_Bsi[1],yerr=[[abs(GK_Bsi[1]-GK_Bsi[2])],[abs(GK_Bsi[1]-GK_Bsi[0])]], fmt='X',color='b',markersize=12)
-plt.errorbar(2.75,Erl[1],yerr=[[abs(Erl[1]-Erl[2])],[abs(Erl[1]-Erl[0])]], fmt='o',color='g',markersize=12)
-plt.errorbar(3.25,GK_Erl[1],yerr=[[abs(GK_Erl[1]-GK_Erl[2])],[abs(GK_Erl[1]-GK_Erl[0])]], fmt='X',color='g',markersize=12)
-plt.errorbar(3.75,Gef[1],yerr=[[abs(Gef[1]-Gef[2])],[abs(Gef[1]-Gef[0])]], fmt='o',color='purple',markersize=12)
-plt.errorbar(4.25,GK_Gef[1],yerr=[[abs(GK_Gef[1]-GK_Gef[2])],[abs(GK_Gef[1]-GK_Gef[0])]], fmt='X',color='purple',markersize=12)
-
-plt.ylim([-8,-37])
-plt.xticks([1,2,3,4],labels)
-plt.ylabel('$\Delta G$ ($k_B T$)', fontsize=16)
-plt.yticks(fontsize=12)
+plt.xlim([-8,-37])
+plt.yticks([1,2,3,4],labels)
+plt.xlabel('$\Delta G$ ($k_B T$)', fontsize=16)
 plt.xticks(fontsize=16)
+plt.yticks(fontsize=20)
 
 plt.title('Src',fontsize=20)
 
@@ -66,4 +78,3 @@ plt.tight_layout()
 
 plt.savefig('Src_delG_compare.png', dpi=500)
 plt.savefig('Src_delG_compare.pdf')
-
--- figures/Figure4/generate_p38_delG_compare.py
+++ figures/Figure4/generate_p38_delG_compare.py
@@ -1,7 +1,7 @@
 #p38
 #Analysis conducted on lilac, here results are taken from output json files
 #quickmodel --inputs inputs_multiple_well_single_wv_p38_4ligs --type singlet --nsamples 100000
-# Just to note: 'P_error'       :  0.35,
+# Just to note: 'P_error'       :  0.15,
 
 import matplotlib.pyplot as plt
 
@@ -11,32 +11,48 @@ sns.set_context('talk')
 
 import matplotlib.lines as mlines
 
-# p38  "DeltaG_cred_int": "$\\Delta G$ =  -14 [-15.5,-12.6] $k_B T$",
-Bos = [-15.5,-14 ,-12.6] 
+# Import 95% credibility intervals from json file quickmodel results
+import json
+Bos_file = 'mwsw/p38-Bosutinib-AB-2019-10-04 09:06.json'
+Bsi_file = 'mwsw/p38-Bosutinib Isomer-CD-2019-10-04 12:13.json'
+Erl_file = 'mwsw/p38-Erlotinib-EF-2019-10-04 15:18.json'
+Gef_file = 'mwsw/p38-Gefitinib-GH-2019-10-04 18:51.json'
 
-# "DeltaG_cred_int": "$\\Delta G$ =  -11.9 [-13,-10.4] $k_B T$",
-Bsi = [-13,-11.9 ,-10.4] 
+def json_parse(file):
+    with open(file) as json_file:
+        data = json.load(json_file)
+    three = data['DeltaG_cred_int'].split("=")[1].split(" $")[0].strip().split("[")
+    first = float(three[0].strip())
+    inner = three[1].split(",")
+    second = float(inner[0])
+    third = float(inner[1][:-1])
+    lig = [third,
+           first,
+           second]
 
-# "DeltaG_cred_int": "$\\Delta G$ =  -10.8 [-12,-9.48] $k_B T$",
-Erl = [-12,-10.8 ,-9.48] 
+    print(lig)
+    return(lig)
 
-#  "DeltaG_cred_int": "$\\Delta G$ =  -12.9 [-14.2,-11.5] $k_B T$",
-Gef = [-14.2,-12.9 ,-11.5]
+Bos = json_parse(Bos_file)
+Bsi = json_parse(Bsi_file)
+Erl = json_parse(Erl_file)
+Gef = json_parse(Gef_file)
+
+labels = ['bosutinib','bosutinib isomer','erlotinib','gefitinib']
 
 
-labels = ['Bos','Bsi','Erl','Gef']
+fig, ax = plt.subplots(figsize=(6,4))
 
-fig, ax = plt.subplots(figsize=(8,4))
+plt.errorbar(Bos[1],1,xerr=[[abs(Bos[1]-Bos[2])],[abs(Bos[1]-Bos[0])]], fmt='o',color='c',markersize=12)
+plt.errorbar(Bsi[1],2,xerr=[[abs(Bsi[1]-Bsi[2])],[abs(Bsi[1]-Bsi[0])]], fmt='o',color='b',markersize=12)
+plt.errorbar(Erl[1],3,xerr=[[abs(Erl[1]-Erl[2])],[abs(Erl[1]-Erl[0])]], fmt='o',color='g',markersize=12)
+plt.errorbar(Gef[1],4,xerr=[[abs(Gef[1]-Gef[2])],[abs(Gef[1]-Gef[0])]], fmt='o',color='purple',markersize=12)
 
-plt.errorbar(1,Bos[1],yerr=[[abs(Bos[1]-Bos[2])],[abs(Bos[1]-Bos[0])]], fmt='o',color='c',markersize=12)
-plt.errorbar(2,Bsi[1],yerr=[[abs(Bsi[1]-Bsi[2])],[abs(Bsi[1]-Bsi[0])]], fmt='o',color='b',markersize=12)
-plt.errorbar(3,Erl[1],yerr=[[abs(Erl[1]-Erl[2])],[abs(Erl[1]-Erl[0])]], fmt='o',color='g',markersize=12)
-plt.errorbar(4,Gef[1],yerr=[[abs(Gef[1]-Gef[2])],[abs(Gef[1]-Gef[0])]], fmt='o',color='purple',markersize=12)
-plt.xticks([1,2,3,4],labels)
-plt.ylim([-8,-37])
-plt.ylabel('$\Delta G$ ($k_B T$)', fontsize=16)
-plt.yticks(fontsize=12)
+plt.xlim([-8,-37])
+plt.yticks([1,2,3,4],labels)
+plt.xlabel('$\Delta G$ ($k_B T$)', fontsize=16)
 plt.xticks(fontsize=16)
+plt.yticks(fontsize=20)
 
 plt.title('p38',fontsize=20)
 
